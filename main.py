9a15886a7137d4fb7ef4ac6fbf9be1882044338d
Azad Kumar Jha <azadkumarjha09@gmail.com>
2025-03-19 21:49:50 +0530

    added all changes

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 from cnnClassifier import logger
-
-from cnnClassifier import logger
-logger.info("welcome to my custom log")
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIntegrationTrainingPipeline
